165f0ce8e5f25f0e1442d778a76e371f759975e6
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-02-15 15:33:33 -0700

    Remove dependent and load_seed

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,6 +1,6 @@
 class Form < ApplicationRecord
   has_many :pet_forms
-  has_many :pets, through: :pet_forms, dependent: :destroy
+  has_many :pets, through: :pet_forms
 
   enum status: ["In Progress", "Pending", "Accepted", "Rejected"] 
   validates :name, :street_address, :city, :state, :zip_code, presence: true
