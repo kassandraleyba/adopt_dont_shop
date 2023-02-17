60f00425b0e544a1d5ad70af501d30901899f06f
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-02-17 07:44:26 -0700

    Add numericality in form model

--- app/models/form.rb
+++ app/models/form.rb
@@ -3,5 +3,6 @@ class Form < ApplicationRecord
   has_many :pets, through: :pet_forms
 
   enum status: ["In Progress", "Pending", "Accepted", "Rejected"] 
-  validates :name, :street_address, :city, :state, :zip_code, presence: true
+  validates :name, :street_address, :city, :state, presence: true
+  validates :zip_code, presence: true, numericality: true
 end
